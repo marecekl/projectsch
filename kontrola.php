67fb939f25510d8d1a1d11e3acda6a2a05bc93e8
marecekl <mareklauko1994@gmail.com>
2017-04-26 11:26:50 +0200

    tabulka

--- kontrola.php
+++ kontrola.php
@@ -1,20 +1,4 @@
 <?php include_once 'school_template/header.php'; ?>	
-<?php
-if(isset($_POST['select1'])){
-    $select1 = $_POST['select1'];
-    switch ($select1) {
-        case 'value1':
-            echo 'this is value1<br/>';
-            break;
-        case 'value2':
-            echo 'value2<br/>';
-            break;
-        default:
-            # code...
-            break;
-    }
-}
-?>
 <!-- Header -->
 	<header id="head">
 		<div class="container">
@@ -44,6 +28,26 @@ if(isset($_POST['select1'])){
     	</select>
     	<input type="submit" name="submit" value="Go"/>
 	</form></center>
+	<?php
+	if(isset($_POST['select1'])){
+    	$select1 = $_POST['select1'];
+    	switch ($select1) {
+        case 'value1':
+            echo '<table style="width:100%"><tr><th>Humanitné</th><th>Spoločenské</th><th>Vedné</th><tr>
+    <td>1: 100-80</td>
+    <td>1: 100-70</td>
+    <td>1: 100-90</td>
+  </tr></tr></table>';
+            break;
+        case 'value2':
+            echo 'value2<br/>';
+            break;
+        default:
+            # code...
+            break;
+    	}
+	}
+?>
 </body>
 
 <?php include_once'school_template/footer.php'; ?>
