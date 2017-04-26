00b6e5d0a7100b02367f8257bf039178012307a2
marecekl <mareklauko1994@gmail.com>
2017-04-26 11:03:06 +0200

    Update kontrola.php

--- kontrola.php
+++ kontrola.php
@@ -27,15 +27,6 @@
   	</select>
   	<input type="submit" value="Submit the form"/>
 	</form>
-	<?php
-		$option = isset($_POST['taskOption']) ? $_POST['taskOption'] : false;
-   		if ($option) {
-     		 echo htmlentities($_POST['taskOption'], ENT_QUOTES, "UTF-8");
-   		} else {
-     		echo "task option is required";
-     		exit; 
-   		}
-	?>
 </body>
 
 <?php include_once'school_template/footer.php'; ?>
