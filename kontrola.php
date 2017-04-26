5f363166907c6dd98e5371384caf9d05b978c9fa
marecekl <mareklauko1994@gmail.com>
2017-04-26 11:38:59 +0200

    Update kontrola.php

--- kontrola.php
+++ kontrola.php
@@ -1,23 +1,4 @@
-<?php include_once 'school_template/header.php'; ?>	
-<!-- Header -->
-	<header id="head">
-		<div class="container">
-                    <div class="heading-text">							
-			<h1 class="animated flipInY delay1">Online pre žiakov</h1> <br/>
-			<p>Kontrolujte si úlohy, rozvrh a známky na jednom mieste!</p>
-                    </div>
-                    <div class="fluid_container">                       
-                        <div class="camera_wrap camera_emboss pattern_1" id="camera_wrap_4">
-                            <div data-thumb="assets/images/slides/thumbs/img1.jpg" data-src="assets/images/slides/img1.jpg">
-                            </div> 
-                            <div data-thumb="assets/images/slides/thumbs/img2.jpg" data-src="assets/images/slides/img2.jpg">
-                            </div>
-                            <div data-thumb="assets/images/slides/thumbs/img3.jpg" data-src="assets/images/slides/img3.jpg">
-                            </div> 
-                        </div><!-- #camera_wrap_3 -->
-                    </div><!-- .fluid_container -->
-		</div>
-</header>
+<?php include_once 'school_template/header.php'; ?>
 <body>
 	<center><form action="" method="post">
     	<select name="select1">
